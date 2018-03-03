3b0e49cbca9911cfd02601cca705f37ce5653de5
tridings <tanner.j.ridings@gmail.com>
2018-03-02 18:13:59 -0600

    blah

--- bin/passwordVault.py
+++ bin/passwordVault.py
@@ -7,24 +7,50 @@ from bcrypt import hashpw, gensalt
 import yaml
 
 userAccountList = []
-accountTypes = []
+filePath = "/home/mint/scripts/python/etc/myYamlFile.yaml"
 
-with open("/home/mint/scripts/python/etc/myYamlFile.yaml") as f:
-    list_doc = yaml.load(f)
+def yaml_loader(filepath):
+    # Loads a yaml file
+    with open (filepath, "r") as file_descriptor:
+        data = yaml.load(file_descriptor)
+    return data
+
+def yaml_query(filepath, request):
+    queryResult = yaml_loader(filepath)[request]
+    return queryResult
+
+def yaml_dump(filepath, data):
+    # Dumps data to a yaml file
+    with open(filepath, "w") as file_descriptor:
+        yaml.dump(data, file_descriptor)
 
 def account_authenticate():
+    record= yaml_loader(filePath)
     username = raw_input(["Please enter username..."])
-    if username in userAccountList:
-        password = getpass.getpass(["Please enter password..."])
-        hashedPassword = hashpw(password, gensalt())
-        if hashedPassword == list_doc[username]["password"]:
-            return True
-            # This should probably be a new function
-
+    # This needs to check against the yaml file "accounts" to see if it exists
+    if username in record['accounts']:
+        password = getpass(["Please enter password..."])
+        hashedPassword = hashpw(password, record['accounts'][username]['salt'])
+        if hashedPassword == record['accounts'][username]['password']:
+            print "Correct password"
+            print ""
+            valid = False
+            while valid == False:                 
+                choice = raw_input(["Would you like to enter a new password record or retrieve a password? (ENTER/RETRIEVE) Or enter EXIT to quit the program"])
+                if choice == "ENTER":
+                    # passwordRecordCreate(choice, username)
+                    valid = True
+                elif choice == "RETRIEVE":
+                    # passwordRecordRetrieve(choice, username)
+                    valid = True
+                elif choice == "EXIT":
+                    exit()
+                else:
+                    print "Invalid choice was entered"
+                    valid = False
         else:
             print "Incorrect Password"
-            return False
-            exit
+            exit()
     else:
         print "Username has not been registered..."
         print ""
@@ -32,13 +58,33 @@ def account_authenticate():
         if choice == "yes":
             create_account(username)
         elif choice == "no":
-            exit
+            exit()
+ 
+def passwordRecordCreate(type, username):
+    choice = "yes"
+    while choice == "yes":
+        yaml_loader(filePath)
+        title = raw_input(["Please enter the title of the password you would like to store... (example: facebook, gmail, ect..."])
+        password = getpass(["Please enter password for %s..." % title])
+        data = {
+            "accounts":{
+                username:{
+                    "titles":{
+                        "title": title,
+                        "password": password
+                    }
+                }
+            }
+        }
+        yaml_dump(filePath, data)
+        choice = raw_input(["Would you like to enter another password? (yes/no)"])
 
-def process_request():
-    if account_authenticate() == True:
-        option = raw_input(["Please enter one of these account passwords that you'd like : %s" % accountTypes])
-        retrievedPassword = list_doc[option]['password']
-        addToClipBoard(retrievedPassword)
+
+def passwordRecordRetrieve(type):
+    accountTypes = yaml_query(filePath, username)
+    option = raw_input(["Please enter one of these account passwords that you'd like : %s" % accountTypes])
+    retrievedPassword = list_doc[option]['password']
+    addToClipBoard(retrievedPassword)
 
 def addToClipBoard(text):
     command = 'echo ' + text.strip() + '| clip'
@@ -50,24 +96,32 @@ def create_account(username):
         newUsername = username
     else:
         newUsername = raw_input(["Please enter new username..."])
-    plaintext_password = getpass(["Please enter new password..."])
+    plainTextPassword = getpass(["Please enter new password..."])
     salt = gensalt()
-    hashed = hashpw(plaintext_password, salt)
-    plaintext_password_check = getpass(["Please reenter new password..."])
-    hashedCheck = hashpw(plaintext_password_check, salt)
+    hashed = hashpw(plainTextPassword, salt)
+    plainTextPasswordCheck = getpass(["Please reenter new password..."])
+    hashedCheck = hashpw(plainTextPasswordCheck, salt)
     # match = "no"
     # while match == "no":
     if hashed == hashedCheck:  
         print "Passwords match"
         userPassword = hashed
-        userAccountList.append(newUsername)
-        list_doc[newUsername]['password'] = hashed
+        # userAccountList.append(newUsername)
+        data = {
+            "accounts":{
+                newUsername:{
+                    "password": hashed,
+                    "salt": salt
+                }
+            }
+        }
+        yaml_loader(filePath)
+        yaml_dump(filePath, data)
         match = "yes"
-        print userAccountList   
     else:
         print "Passwords do not match"
         match = "no" 
-        exit       
+        exit()  
 
 def main():
     account_authenticate()
